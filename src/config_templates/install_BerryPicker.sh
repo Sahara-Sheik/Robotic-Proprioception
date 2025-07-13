cb4851141427e55efef63203ec2a0a5946b13599
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-07-13 10:45:05 -0400

    Created Installation script

---
+++ src/config_templates/install_BerryPicker.sh
@@ -0,0 +1,25 @@
+# create the config
+mkdir -p ~/.config/BerryPicker
+echo configpath: \"~/WORK/BerryPicker/conf/settings.yaml\" > .config/BerryPicker/mainsettings.yaml
+
+# check out the src
+mkdir -p ~/WORK/BerryPicker/src 
+cd ~/WORK/BerryPicker/src/
+git clone https://github.com/lboloni/BerryPicker
+git clone https://github.com/julian-8897/Conv-VAE-PyTorch
+# check out the 
+
+# create the data dirs
+mkdir -p ~/WORK/BerryPicker/data
+
+# create the config
+mkdir -p ~/WORK/BerryPicker/cfg
+cd ~/WORK/BerryPicker/cfg
+cp ~/WORK/BerryPicker/src/config_templates/settings_sample.yaml settings.yaml
+
+# create the vm
+mkdir -p ~/WORK/BerryPicker/vm
+cd ~/WORK/BerryPicker/vm
+python -m venv berrypickervenv
+source berrypickervenv/bin/activate
+pip install 
---
+++ src/config_templates/uninstall_BerryPicker.sh
@@ -0,0 +1,3 @@
+echo on
+rm -rf ~/.config/BerryPicker
+rm -rf ~/WORK/BerryPicker
