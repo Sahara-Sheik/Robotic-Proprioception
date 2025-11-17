7d2a48230e3bc78a93958e1cc465e191d4691657
sahara-sheik <sahara.shekholeslami@gmail.com>
2025-11-17 11:24:35 -0500

    changed the file copy in explore demopack  and addressed ~ in the paths

--- src/sensorprocessing/converter.py
+++ src/sensorprocessing/converter.py
@@ -2,7 +2,7 @@ import nbformat
 from nbconvert import PythonExporter
 
 # Load the notebook
-with open('/home/ssheikholeslami/BerryPicker/src/visual_proprioception/Compare_VisualProprioception_multiview.ipynb') as f:
+with open('/home/sa641631/WORK/BerryPicker/src/BerryPicker/src/demonstration/Explore_Demopack.ipynb') as f:
     notebook = nbformat.read(f, as_version=4)
 
 # Convert to Python
@@ -10,5 +10,5 @@ python_exporter = PythonExporter()
 python_code, _ = python_exporter.from_notebook_node(notebook)
 
 # Write to a Python file
-with open('Train_VisualProprioception_multiview.py', 'w') as f:
+with open('/home/sa641631/WORK/BerryPicker/src/BerryPicker/src/demonstration/Explore_Demopack.py', 'w') as f:
     f.write(python_code)
