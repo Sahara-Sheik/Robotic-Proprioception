7f021fc22a17abe28b8486c791d4064cfa28cacd
sahara-sheik <sahara.shekholeslami@gmail.com>
2025-11-18 06:33:59 -0500

    flow added

--- src/sensorprocessing/converter.py
+++ src/sensorprocessing/converter.py
@@ -2,7 +2,7 @@ import nbformat
 from nbconvert import PythonExporter
 
 # Load the notebook
-with open('/home/sa641631/WORK/BerryPicker/src/BerryPicker/src/visual_proprioception/Train_VisualProprioception.ipynb') as f:
+with open('/home/sa641631/WORK/BerryPicker/src/BerryPicker/src/visual_proprioception/Flow_VisualProprioception.ipynb') as f:
     notebook = nbformat.read(f, as_version=4)
 
 # Convert to Python
@@ -10,5 +10,5 @@ python_exporter = PythonExporter()
 python_code, _ = python_exporter.from_notebook_node(notebook)
 
 # Write to a Python file
-with open('/home/sa641631/WORK/BerryPicker/src/BerryPicker/src/visual_proprioception/Train_VisualProprioception.py', 'w') as f:
+with open('/home/sa641631/WORK/BerryPicker/src/BerryPicker/src/visual_proprioception/Flow_VisualProprioception.py', 'w') as f:
     f.write(python_code)
